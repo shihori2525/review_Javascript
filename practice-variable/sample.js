a237919c11ba4ab59cfd7105934cb8d1a31179da
shihori2525 <rirakkuma.250@gmail.com>
2022-04-15 22:42:23 +0900

    [Add]prompt function

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -1,3 +1,11 @@
+let user_hand = pronpt('じゃんけんの手をグー、チョキ、パーから選んでください');
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand,js_hand);
+
+alert('あなたの選んだ手は' + user_hand +'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
 function getJShand(){
   let js_hand_num = Math.floor(Math.random()*3);
 
