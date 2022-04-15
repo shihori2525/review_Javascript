ec07f76524fc4428c9a02c815d357d8af5e0362c
shihori2525 <rirakkuma.250@gmail.com>
2022-04-15 22:28:32 +0900

    [Fix]js file

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -1,3 +1,13 @@
-let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random()*3);
 
-alert('あなたの選んだ手は'+ user_hand +'です。');
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  }else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  }else if(js_hand_num == 2)
+    js_hand = "パー";
+  }
+
+  return js_hand;
+}
