5597d0fc7fc5ab7c202b819697f00bcf7dd2847b
shihori2525 <rirakkuma.250@gmail.com>
2022-04-16 22:37:20 +0900

    [Add]Bifurcation

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -8,14 +8,47 @@ alert('あなたの選んだ手は' + user_hand +'です。\nJavaScriptの選ん
 
 function getJShand(){
   let js_hand_num = Math.floor(Math.random()*3);
+  let hand_name;
 
   if(js_hand_num == 0){
-    js_hand = "グー";
+    hand_name = "グー";
   }else if(js_hand_num == 1){
-    js_hand = "チョキ";
+    hand_name = "チョキ";
   }else if(js_hand_num == 2)
-    js_hand = "パー";
+    hand_name = "パー";
   }
 
-  return js_hand;
+  return hand_name;
+}
+
+function winLose(user,js){
+  let winLoseStr,
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    }else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    }else if(js == "パー"){
+      winLoseStr = "負け"
+    }
+  }else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    }else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    }else if(js == "パー"){
+      winLoseStr = "勝ち"
+    }
+  }else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    }else if(js == "チョキ"){
+      winLoseStr = "負け";
+    }else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
 }
