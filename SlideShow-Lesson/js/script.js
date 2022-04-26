54515e40b5dad15d87c59c372d2c80c113d48b84
shihori2525 <rirakkuma.250@gmail.com>
2022-04-26 20:18:00 +0900

    [Add]js

---
+++ SlideShow-Lesson/js/script.js
@@ -0,0 +1,12 @@
+$(".the Target").skippr({
+  transition :'fade',
+  speed :100,
+  easing :'easeOutQuart',
+  navType :'block',
+  childrenElementType :'div',
+  arrows :true,
+  autPlay :false,
+  autoPlayDuration :5000,
+  keyboardOnAlways :true,
+  hidePrevious :false
+});
