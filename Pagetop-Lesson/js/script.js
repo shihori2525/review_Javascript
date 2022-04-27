83abec08034af4811da16494a083a0ab6608e8dd
shihori2525 <rirakkuma.250@gmail.com>
2022-04-27 21:46:12 +0900

    [Add]file

---
+++ Pagetop-Lesson/js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition :'fade',
+  speed :100,
+  easing :'easeOutQuart',
+  navType :'block',
+  childrenElementType :'div',
+  arrows :true,
+  autoPlay :true,
+  autoPlayDuration :5000,
+  keyboardOnAlways :true,
+  hidePrevious :false
+});
